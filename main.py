e55056ba7cdb3c06a20cdd7caf15412d2d9f9ffc
Gusseppe <hackersvlc@gmail.com>
2023-12-13 18:02:56 -0500

    password

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import necessary modules
 import streamlit as st
+import hmac
 # from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -27,7 +28,33 @@ from glob import glob
 #     "./atencion_sabados.txt",
 #     ""
 # ]
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+    
 paths = glob("./information/*.txt")
 
 loaders = [TextLoader(file_path=path, encoding='utf8') for path in paths]
@@ -105,7 +132,7 @@ final_inputs = {
 }
 # And finally, we do the part that returns the answers
 answer = {
-    "answer": final_inputs | ANSWER_PROMPT | ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0),
+    "answer": final_inputs | ANSWER_PROMPT | ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0),
     "docs": itemgetter("docs"),
 }
 # And now we put it all together!
